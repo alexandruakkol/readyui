c128956f77db32a4b061641612ba2840512c3f68
alexandruakkol <alexandru.akkol@gmail.com>
2024-03-10 18:00:40 -0700

    init

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const ResponsiveNavbar = (props) => {
+    const {items, logo_el} = props;
+  return (
+    <nav style={styles.nav}>
+        <>{logo_el}</>
+        <div style={styles.div}>
+            {items}
+        </div>
+    </nav>
+  )
+}
+
+const styles = {
+    nav:{
+        userSelect: 'none',
+        paddingLeft: '2vw',
+        display: 'flex',
+        height: '7vh',
+        boxShadow: '0 1px 1px var(--BORDERS)',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '90%'
+    },
+    div:{
+        gap: '33px',
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'center'
+    }
+}
+
+
+export default ResponsiveNavbar
