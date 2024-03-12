dcd87739213915d858a5fa87dd36a4690a7709d8
alexandruakkol <alexandru.akkol@gmail.com>
2024-03-11 17:23:38 -0700

    resizing

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './Navbar.css';
 import { GiHamburgerMenu } from "react-icons/gi";
 
@@ -8,10 +8,31 @@ const Navbar = (props) => {
     
     const [isListShown, setIsListShown] = useState(false);
     const reactElement = React.Children.only(cta_button);
+    const [logo, setLogo] = useState(small_logo);
+
+    useEffect(() => {
+        const handleResize = () => {
+            const screenWidth = window.innerWidth;
+            if (screenWidth >= 1000) {
+                setLogo(large_logo);
+            }
+            else {
+                setLogo(small_logo);
+            }
+        }
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+    },[]);
+
 
     return (
     <nav className='rui-navbar'>
-        <div className="rui-logo">{large_logo}</div>
+        <div className="rui-logo">{logo}</div>
         <div className='rui-hamburger-container'>
             <GiHamburgerMenu className="rui-hamburger" onClick={() => {setIsListShown(!isListShown)}}/>
             <div className={`rui-hamburger-list ${isListShown ? 'open' : ''}`}>
